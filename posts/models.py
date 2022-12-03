df6233340bc3653d43d0b973e5aa868d6d924376
Ayana2006 <borubaevaayan@gmail.com>
2022-12-03 18:52:21 +0600

    first commit

---
+++ posts/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from users.models import User
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(upload_to="post_images/")
+    created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, related_name='post_user', on_delete = models.CASCADE)
+    
+    def __str__(self):
+        return f'Заголовок:{self.title}, Описание:{self.description}'
+    
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+    
+    
+    
